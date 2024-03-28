c17d6987171c831203c5443222e9fc6c188f1f3f
ExtropyRobotics <124261167+ExtropyRobotics@users.noreply.github.com>
2024-03-28 15:36:57 +0200

    pt mario final build

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024_2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024_2025.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name="motoare")
+@TeleOp(name="Teleop-Incepatori")
 
-public class TeleOp extends LinearOpMode {
+public class TeleOp2024_2025 extends LinearOpMode {
     DcMotor motorFataStanga = null;
     DcMotor motorFataDreapta = null;
     DcMotor motorSpateStanga = null;
@@ -15,6 +16,10 @@ public class TeleOp extends LinearOpMode {
     DcMotor motor1 = null;
     DcMotor motor2 = null;
 
+    Servo servoSus1 = null;
+    Servo servoSus2 =null;
+    Servo servoJos1 =null;
+    Servo servoJos2 =null;
         double sticklefty = 0;
         double stickleftx = 0;
         double stickrightx = 0;
@@ -27,9 +32,13 @@ public class TeleOp extends LinearOpMode {
         motorFataDreapta = hardwareMap.get(DcMotor.class, "motorFataDreapta");
         motorSpateStanga = hardwareMap.get(DcMotor.class, "motorSpateStanga");
         motorSpateDreapta = hardwareMap.get(DcMotor.class, "motorSpateDreapta");
-        motorSpateDreapta = hardwareMap.get(DcMotor.class, "motorSpateDreapta");
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
         motor2 = hardwareMap.get(DcMotor.class, "motor2");
+
+        servoSus1 = hardwareMap.get(Servo.class, "servoSus1");
+        servoSus2 = hardwareMap.get(Servo.class, "servoSus2");
+        servoJos1 = hardwareMap.get(Servo.class, "servoJos1");
+        servoJos2 = hardwareMap.get(Servo.class, "servoJos2");
 
         motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -40,22 +49,25 @@ public class TeleOp extends LinearOpMode {
         motorSpateStanga.setDirection(DcMotorSimple.Direction.REVERSE);
         motorFataStanga.setDirection(DcMotorSimple.Direction.REVERSE);
         motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        servoJos2.setDirection(Servo.Direction.REVERSE);
+
 
         waitForStart();
 
         while (opModeIsActive()){
-            sticklefty= -gamepad1.left_stick_y;
-            stickleftx= gamepad1.left_stick_x;
-            stickrightx= gamepad1.right_stick_x;
+            sticklefty = -gamepad1.left_stick_y;
+            stickleftx = gamepad1.left_stick_x;
+            stickrightx = gamepad1.right_stick_x;
 
             motorFataStanga.setPower(sticklefty+stickleftx+stickrightx);
             motorFataDreapta.setPower(sticklefty-stickleftx-stickrightx);
             motorSpateDreapta.setPower(sticklefty+stickleftx-stickrightx);
             motorSpateStanga.setPower(sticklefty-stickleftx+stickrightx);
-            if(gamepad2.left_stick_y>0){
-                targetPoz+=1;
+
+            if(gamepad2.left_stick_y > 0){
+                targetPoz += 1;
             }
-            if(gamepad2.left_stick_y<0) {
+            if(gamepad2.left_stick_y < 0){
                 targetPoz -= 1;
             }
             motor1.setTargetPosition(targetPoz);
@@ -64,6 +76,10 @@ public class TeleOp extends LinearOpMode {
             motor2.setPower(0.4);
             motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            servoJos1.setPosition(0.2);
+            servoJos2.setPosition(0.2);
         }
+
     }
 }
