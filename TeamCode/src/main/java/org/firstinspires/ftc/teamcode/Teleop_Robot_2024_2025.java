dabd316c3b7950e73685fb3d0662d5d6aa376534
27SGMario <marcumaria1985@gmail.com>
2024-04-06 12:50:08 +0300

    stagiu 3 robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_Robot_2024_2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_Robot_2024_2025.java
@@ -10,27 +10,28 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Teleop_Robot_2024_2025 extends LinearOpMode {
 
-    DcMotor motorStangaFata=null;
-    DcMotor motorDreaptaFata=null;
-    DcMotor motorStangaSpate=null;
-    DcMotor motorDreaptaSpate=null;
-    DcMotor motorBrat1=null;
-    DcMotor motorBrat2=null;
+    DcMotor motorStangaFata = null;
+    DcMotor motorDreaptaFata = null;
+    DcMotor motorStangaSpate = null;
+    DcMotor motorDreaptaSpate = null;
+    DcMotor motorBrat1 = null;
+    DcMotor motorBrat2 = null;
     Servo servoSus1 = null;
-    Servo servoSus2 =null;
-    Servo servoJos1 =null;
-    Servo servoJos2 =null;
+    Servo servoSus2 = null;
+    Servo servoJos1 = null;
+    Servo servoJos2 = null;
     double sticklefty = 0;
     double stickleftx = 0;
     double stickrightx = 0;
 
     int targetPoz = 0;
+
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         motorStangaFata = hardwareMap.get(DcMotor.class, "motorStangaFata");
         motorDreaptaFata = hardwareMap.get(DcMotor.class, "motorDreaptaFata");
         motorStangaSpate = hardwareMap.get(DcMotor.class, "motorStangaSpate");
-        motorDreaptaSpate = hardwareMap.get(DcMotor.class, "motorDreaptaSpate" );
+        motorDreaptaSpate = hardwareMap.get(DcMotor.class, "motorDreaptaSpate");
         motorBrat1 = hardwareMap.get(DcMotor.class, "motorBrat1");
         motorBrat2 = hardwareMap.get(DcMotor.class, "motorBrat2");
 
@@ -54,53 +55,57 @@ public class Teleop_Robot_2024_2025 extends LinearOpMode {
 
         waitForStart();
 
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
             sticklefty = -gamepad1.left_stick_y;
             stickleftx = gamepad1.left_stick_x;
             stickrightx = gamepad1.right_stick_x;
 
 
-            if(gamepad2.x){
+            if (gamepad2.x) {
                 servoSus1.setPosition(1);
                 servoSus2.setPosition(1);
             }
-            if(gamepad2.a){
+            if (gamepad2.a) {
                 servoSus1.setPosition(0.7);
                 servoSus2.setPosition(0.55);
             }
-            if(gamepad2.left_stick_y<0){
-                   servoJos1.setPosition(0.5);
-                    servoJos2.setPosition(0.5);
-           }
-            if(gamepad2.left_stick_y>0){
-                servoJos1.setPosition(0);
-                servoJos2.setPosition(0);
-                    }
+//            if (gamepad2.left_stick_y < 0) {
+//                servoJos1.setPosition(0.5);
+//                servoJos2.setPosition(0.5);
+//            }
+//            if (gamepad2.left_stick_y > 0) {
+//                servoJos1.setPosition(0.25);
+//                servoJos2.setPosition(0.25);
+//            }
 
 
+            motorStangaFata.setPower(sticklefty + stickleftx + stickrightx);
+            motorDreaptaFata.setPower(sticklefty - stickleftx - stickrightx);
+            motorDreaptaSpate.setPower(sticklefty + stickleftx - stickrightx);
+            motorStangaSpate.setPower(sticklefty - stickleftx + stickrightx);
 
-            motorStangaFata.setPower(sticklefty+stickleftx+stickrightx);
-            motorDreaptaFata.setPower(sticklefty-stickleftx-stickrightx);
-            motorDreaptaSpate.setPower(sticklefty+stickleftx-stickrightx);
-            motorStangaSpate.setPower(sticklefty-stickleftx+stickrightx);
-
-            if(gamepad2.right_stick_y > 0){
+            if (gamepad2.right_stick_y < 0) {
                 targetPoz += 1;
             }
-            if(gamepad2.right_stick_y < 0){
+            if (gamepad2.right_stick_y > 0) {
                 targetPoz -= 1;
             }
             motorBrat1.setTargetPosition(targetPoz);
             motorBrat2.setTargetPosition(targetPoz);
-            motorBrat1.setPower(0.5);
-            motorBrat2.setPower(0.5);
+            motorBrat1.setPower(0.55);
+            motorBrat2.setPower(0.55);
             motorBrat1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             motorBrat2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
+            if (gamepad2.right_stick_y < 0){
+                servoJos2.setPosition(0.5);
+                servoJos1.setPosition(0.5);}
+                if (gamepad2.right_stick_y > 0){
+                    servoJos2.setPosition(0.25);
+                    servoJos1.setPosition(0.25);
+                }
 
 
         }
     }
-
 
 }
