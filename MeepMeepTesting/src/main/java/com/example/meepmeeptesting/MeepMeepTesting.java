56ea1d396c64f2a8c3fb3098b04d8c9c0ce4a464
27SGMario <146442470+27SGMario@users.noreply.github.com>
2025-01-05 13:01:28 +0200

    Autonomie Progres

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,97 +14,11 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
                 .build();
-        RoadRunnerBotEntity rightRed = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
-                .build();
-        RoadRunnerBotEntity leftBlue = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
-                .build();
-        RoadRunnerBotEntity rightBlue = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
-                .build();
-        leftRed.runAction(leftRed.getDrive().actionBuilder(new Pose2d(-36, -60,Math.toRadians(90)))
-                .splineToSplineHeading(new Pose2d(-5, -36, Math.toRadians(90)), Math.toRadians(0))
 
-                .setTangent(Math.toRadians(180))
-                .splineToSplineHeading(new Pose2d(-50, -40, Math.toRadians(90)), Math.toRadians(180))
-
-                .setTangent(Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(-50,-50,Math.toRadians(235)),Math.toRadians(-90))
-                .build());
-
-        rightRed.runAction(rightRed.getDrive().actionBuilder(new Pose2d(12, -60,Math.toRadians(90)))
-                .splineToSplineHeading(new Pose2d(12, -34, Math.toRadians(90)), Math.toRadians(90))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(0))
-                .splineToSplineHeading(new Pose2d(47, -34, Math.toRadians(90)), Math.toRadians(0))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(-170))
-                .splineToSplineHeading(new Pose2d(-56, -58, Math.toRadians(220)), Math.toRadians(-170))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(10))
-                .splineToSplineHeading(new Pose2d(58, -36, Math.toRadians(90)), Math.toRadians(10))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(-170))
-                .splineToSplineHeading(new Pose2d(-56, -58, Math.toRadians(220)), Math.toRadians(-170))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(15))
-                .splineToSplineHeading(new Pose2d(58, -25, Math.toRadians(0)), Math.toRadians(15))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(-165))
-                .splineToSplineHeading(new Pose2d(-56, -58, Math.toRadians(220)), Math.toRadians(-165))
-                .waitSeconds(0.5)
-                .build());
-
-        leftBlue.runAction(leftBlue.getDrive().actionBuilder(new Pose2d(36, 61,Math.toRadians(-90)))
-                        .setTangent(Math.toRadians(-120))
-                .splineToSplineHeading(new Pose2d(9, 36, Math.toRadians(-90)), Math.toRadians(-120))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(0))
-                .splineToSplineHeading(new Pose2d(47, 36, Math.toRadians(-90)), Math.toRadians(0))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(75))
-                .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(70))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(58, 34, Math.toRadians(-90)), Math.toRadians(-90))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(90))
-                .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(90))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(58, 25, Math.toRadians(0)), Math.toRadians(-90))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(90))
-                .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(90))
-                .waitSeconds(0.5)
-                .setTangent(Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(58, 12, Math.toRadians(180)), Math.toRadians(-90))
-                .build());
-
-        rightBlue.runAction(rightBlue.getDrive().actionBuilder(new Pose2d(-10, 60,Math.toRadians(-90)))
-                        .splineToSplineHeading(new Pose2d(-10, 36, Math.toRadians(-90)), Math.toRadians(-90))
-                        .waitSeconds(0.5)
-                        .setTangent(Math.toRadians(180))
-                        .splineToSplineHeading(new Pose2d(-47, 36, Math.toRadians(-90)), Math.toRadians(-180))
-                        .waitSeconds(0.5)
-                        .setTangent(Math.toRadians(15))
-                        .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(15))
-                        .waitSeconds(0.5)
-                        .setTangent(Math.toRadians(-170))
-                        .splineToSplineHeading(new Pose2d(-58, 36, Math.toRadians(-90)), Math.toRadians(-170))
-                        .waitSeconds(0.5)
-                        .setTangent(Math.toRadians(15))
-                        .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(15))
-                        .waitSeconds(0.5)
-                        .setTangent(Math.toRadians(-165))
-                        .splineToSplineHeading(new Pose2d(-58, 27, Math.toRadians(180)), Math.toRadians(-165))
-                        .waitSeconds(0.5)
-                        .setTangent(Math.toRadians(17))
-                        .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(17))
+        leftRed.runAction(leftRed.getDrive().actionBuilder(new Pose2d(5, -57,Math.toRadians(90)))
+                        .splineToSplineHeading(new Pose2d(5, -36, Math.toRadians(90)), Math.toRadians(90))
+                        .setTangent(Math.toRadians(-30))
+                        .splineToSplineHeading(new Pose2d(30,-36,Math.toRadians(90)),Math.toRadians(30))
                         .build());
 
 
@@ -112,9 +26,6 @@ public class MeepMeepTesting {
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                         .addEntity(leftRed)
-//                        .addEntity(rightRed)
-//                        .addEntity(leftBlue)
-//                        .addEntity(rightBlue)
                 .start();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeftRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeftRed.java
@@ -27,15 +27,14 @@ public class AutoLeftRed extends LinearOpMode {
 
 
     Point detection = new Point(0,0);
-    Pose2d startingPose = new Pose2d(-36, -60,Math.toRadians(90));
+    Pose2d startingPoseLeftRed = new Pose2d(-36, -60,Math.toRadians(90));
+    Pose2d startingPoseRightBlue = new Pose2d(5, -57,Math.toRadians(90));
 
     @Override public void runOpMode(){
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         sliderMob = hardwareMap.get(DcMotor.class, "slidermobil");
-
         motorAx = hardwareMap.get(DcMotor.class, "motorax");
-
         cleste = hardwareMap.get(Servo.class, "cleste");
 
         motorAx.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -50,45 +49,34 @@ public class AutoLeftRed extends LinearOpMode {
 
         cleste.setPosition(0);
 
-        drive.setPoseEstimate(startingPose);
-        TrajectorySequence leftRed = drive.trajectorySequenceBuilder(startingPose)
+        TrajectorySequence leftRed = drive.trajectorySequenceBuilder(startingPoseLeftRed)
                 .splineToSplineHeading(new Pose2d(-5, -36, Math.toRadians(90)), Math.toRadians(0))
                 .UNSTABLE_addTemporalMarkerOffset(0,()->{
                     cleste.setPosition(0);
-                    motorAx.setTargetPosition(-470);
-                    motorAx.setPower(0.5);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setTargetPosition(-1900);
-                    sliderMob.setPower(0.7);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    while (sliderMob.getCurrentPosition() > -1900 + 10) sleep(1);
+                    setRotate(-470,0.5);
+                    setSlide(-1900,0.7);
                 })
-                .waitSeconds(0.5)
+                .waitSeconds(2)
                 .UNSTABLE_addTemporalMarkerOffset(-0.20, ()->{
-                    motorAx.setTargetPosition(-410);
-                    motorAx.setPower(0.5);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(-410,0.5);
                 })
                 .waitSeconds(1)
                 .UNSTABLE_addTemporalMarkerOffset(0.4, ()->{
                     cleste.setPosition(0.6);
                 })
                 .UNSTABLE_addTemporalMarkerOffset(-0.1,()->{
-                    sliderMob.setTargetPosition(0);
-                    sliderMob.setPower(0.8);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setSlide(0,0.8);
                 })
                 .waitSeconds(1.5)
                 .setTangent(Math.toRadians(180))
                 .splineToSplineHeading(new Pose2d(-50, -40, Math.toRadians(90)), Math.toRadians(180))
                 .UNSTABLE_addTemporalMarkerOffset(-1.3, ()->{
-                    motorAx.setTargetPosition(30);
-                    sliderMob.setTargetPosition(-1300);
-                    motorAx.setPower(0.5);
-                    sliderMob.setPower(0.8);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(30,0.3);
+                    setSlide(-1300,0.8);
                     cleste.setPosition(0.6);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(-0.6, ()->{
+                    setRotate(30,0);
                 })
                 .waitSeconds(0.5)
                 .UNSTABLE_addTemporalMarkerOffset(-0.2, ()->{
@@ -96,20 +84,14 @@ public class AutoLeftRed extends LinearOpMode {
                 })
                 .waitSeconds(0.5)
                 .UNSTABLE_addTemporalMarkerOffset(-0.3, ()->{
-                    motorAx.setTargetPosition(-470);
-                    sliderMob.setTargetPosition(-500);
-                    motorAx.setPower(0.5);
-                    sliderMob.setPower(0.8);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(-470,0.5);
+                    setSlide(-500,0.8);
                 })
                 .waitSeconds(0.5)
                 .setTangent(Math.toRadians(-90))
                 .splineToSplineHeading(new Pose2d(-52,-52,Math.toRadians(215)),Math.toRadians(-90))
                 .UNSTABLE_addTemporalMarkerOffset(-0.7,()->{
-                    sliderMob.setTargetPosition(-2600);
-                    sliderMob.setPower(0.8);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setSlide(-2600,0.8);
                 })
                 .waitSeconds(1)
                 .UNSTABLE_addTemporalMarkerOffset(0,()->{
@@ -117,30 +99,22 @@ public class AutoLeftRed extends LinearOpMode {
                 })
                 .waitSeconds(1)
                 .UNSTABLE_addTemporalMarkerOffset(-0.3, ()->{
-                    motorAx.setTargetPosition(-470);
-                    sliderMob.setTargetPosition(-500);
-                    motorAx.setPower(0.5);
-                    sliderMob.setPower(0.8);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(-470,0.5);
+                    setSlide(-500,0.8);
                 })
                 .setTangent(Math.toRadians(180))
                 .turn(Math.toRadians(235))
                 .UNSTABLE_addTemporalMarkerOffset(-0.8, ()->{
-                    motorAx.setTargetPosition(0);
-                    sliderMob.setTargetPosition(-1200);
-                    motorAx.setPower(0.5);
-                    sliderMob.setPower(0.8);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    cleste.setPosition(0.6);
+                    setRotate(0,0.3);
+                    setSlide(-1300,0.8);
+                })
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    setRotate(0, 0);
                 })
                 .setTangent(Math.toRadians(90))
                 .splineToSplineHeading(new Pose2d(-60, -40, Math.toRadians(90)), Math.toRadians(180))
                 .UNSTABLE_addTemporalMarkerOffset(0,()->{
-                    motorAx.setTargetPosition(30);
-                    motorAx.setPower(0.5);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(30,0);
                 })
                 .waitSeconds(0.5)
                 .UNSTABLE_addTemporalMarkerOffset(0,()->{
@@ -148,20 +122,14 @@ public class AutoLeftRed extends LinearOpMode {
                 })
                 .waitSeconds(1)
                 .UNSTABLE_addTemporalMarkerOffset(-0.3, ()->{
-                    motorAx.setTargetPosition(-470);
-                    sliderMob.setTargetPosition(-500);
-                    motorAx.setPower(0.5);
-                    sliderMob.setPower(0.8);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(-470,0.5);
+                    setSlide(-500,0.8);
                 })
                 .waitSeconds(0.5)
                 .setTangent(Math.toRadians(-45))
                 .splineToSplineHeading(new Pose2d(-52,-52,Math.toRadians(215)),Math.toRadians(-90))
                 .UNSTABLE_addTemporalMarkerOffset(-0.7,()->{
-                    sliderMob.setTargetPosition(-2600);
-                    sliderMob.setPower(0.8);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setSlide(-2600,0.8);
                 })
                 .waitSeconds(1)
                 .UNSTABLE_addTemporalMarkerOffset(0,()->{
@@ -169,52 +137,105 @@ public class AutoLeftRed extends LinearOpMode {
                 })
                 .waitSeconds(1)
                 .UNSTABLE_addTemporalMarkerOffset(-0.3, ()->{
-                    motorAx.setTargetPosition(-470);
-                    sliderMob.setTargetPosition(-500);
-                    motorAx.setPower(0.5);
-                    sliderMob.setPower(0.8);
-                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    setRotate(-470,0.5);
+                    setSlide(-500,0.8);
+                })
+                .waitSeconds(0.5)
+                .setTangent(Math.toRadians(90))
+                .splineToSplineHeading(new Pose2d(-30, -10, Math.toRadians(0)), Math.toRadians(0))
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    setSlide(-2000, 0.8);
                 })
                 .waitSeconds(5)
-
-
-//                .UNSTABLE_addTemporalMarkerOffset(0.3,()->{
-//                    cleste.setPosition(0);
-//                }).waitSeconds(1)
-//                .setTangent(Math.toRadians(15))
-//                .splineToSplineHeading(new Pose2d(58, 58, Math.toRadians(45)), Math.toRadians(15))
-//                .UNSTABLE_addTemporalMarkerOffset(0.5,()->{
-//                    motorAx.setTargetPosition(-500);
-//                    motorAx.setPower(0.5);
-//                    motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                    sliderMob.setTargetPosition(-5000);
-//                    sliderMob.setPower(0.7);
-//                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                    while (sliderMob.getCurrentPosition() > -5000 + 10) sleep(1);
-//                })
-//                .waitSeconds(0.5)
-//                .UNSTABLE_addTemporalMarkerOffset(0.5,()->{
-//                  cleste.setPosition(0.09);
-//                })
-//                .UNSTABLE_addTemporalMarkerOffset(0.6,()->{
-//                    sliderMob.setTargetPosition(0);
-//                    sliderMob.setPower(0.8);
-//                    sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                    while (sliderMob.getCurrentPosition() < 10) sleep(1);
-//                }).waitSeconds(1)
-//                .setTangent(Math.toRadians(-170))
-//                .splineToSplineHeading(new Pose2d(-58, 36, Math.toRadians(-90)), Math.toRadians(-170))
-//                .UNSTABLE_addTemporalMarkerOffset(0.8,()->{
-//                    cleste.setPosition(0);
-//                })
-//                .waitSeconds(0.5)
-
                 .build();
 
 
+
+
+
+
+
+
+        TrajectorySequence rightBlue = drive.trajectorySequenceBuilder(startingPoseRightBlue)
+                .splineToSplineHeading(new Pose2d(5, -32, Math.toRadians(90)), Math.toRadians(90))
+                .UNSTABLE_addTemporalMarkerOffset(0,()->{
+                    cleste.setPosition(0);
+                    setRotate(-470,0.5);
+                    setSlide(-1900,0.7);
+                })
+                .waitSeconds(2)
+                .UNSTABLE_addTemporalMarkerOffset(-0.20, ()->{
+                    setRotate(-400,0.5);
+                })
+                .waitSeconds(1)
+                .UNSTABLE_addTemporalMarkerOffset(-0.1,()->{
+                    setSlide(0,0.8);
+                })
+                .waitSeconds(0.5)
+                .UNSTABLE_addTemporalMarkerOffset(0.4, ()->{
+                    cleste.setPosition(0.6);
+                })
+                .waitSeconds(1)
+                .setTangent(0)
+                .splineToSplineHeading(new Pose2d(49,-32,Math.toRadians(90)),Math.toRadians(30))
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    setSlide(-650, 0.8);
+                    setRotate(30, 0.8);
+                })
+                .waitSeconds(0.5)
+                .setTangent(Math.toRadians(180))
+                .splineToSplineHeading(new Pose2d(45,-32,Math.toRadians(90)),Math.toRadians(180))
+                .waitSeconds(0.5)
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    setRotate(0, 0);
+                })
+                .waitSeconds(0.5)
+                .UNSTABLE_addTemporalMarkerOffset(0.4, ()->{
+                    cleste.setPosition(0);
+                })
+                .waitSeconds(1)
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    setRotate(-30, 0.5);
+                    setSlide(-2200, 0.5);
+                })
+                .waitSeconds(0.5)
+                .splineToSplineHeading(new Pose2d(45,-33,Math.toRadians(270)),Math.toRadians(270))
+                .waitSeconds(0.5)
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    cleste.setPosition(0.6);
+                    setSlide(0, 0.5);
+                })
+                .waitSeconds(0.5)
+                .UNSTABLE_addTemporalMarkerOffset(0, ()->{
+                    setRotate(-200, 0.5);
+                })
+                .waitSeconds(0.5)
+                .turn(Math.toRadians(180))
+                .setTangent(0)
+                .splineToSplineHeading(new Pose2d(54,-33,Math.toRadians(90)),Math.toRadians(0))
+                .waitSeconds(1)
+                .build();
+
+
+        drive.setPoseEstimate(startingPoseRightBlue);
         waitForStart();
-        drive.followTrajectorySequence(leftRed);
+        drive.followTrajectorySequence(rightBlue);
 
     }
+
+
+
+
+
+    public void setRotate(int x,double pow){
+        motorAx.setTargetPosition(x);
+        motorAx.setPower(pow);
+        motorAx.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void setSlide(int x,double pow){
+        sliderMob.setTargetPosition(x);
+        sliderMob.setPower(pow);
+        sliderMob.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
 }
