ef2bb3001648ef052b5567631a231692a3bc0354
HoxhaArjana <arjanahoxha548@gmail.com>
2025-02-18 18:53:52 +0200

    RegioRight brat

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RegioRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RegioRight.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -12,22 +13,24 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 
 @Autonomous (name = "! RegioRight")
+@Config
 public class RegioRight extends LinearOpMode {
-    Pose2d startingPoseRegioRight = new Pose2d(10, -57, Math.toRadians(90));
-    int targetAx = 0;
-    int targetSlider = 0;
-    double close = 0.5;
-    double open = 0.4;
-    double clawPoz = close;
-    double wristPlace = 0.1;
-    double maxVel = 25;
+    public Pose2d startingPoseRegioRight = new Pose2d(10, -57, Math.toRadians(90));
+    public int targetAx = 0;
+    public int targetSlider = 0;
+    public double close = 0.5;
+    public double open = 0.4;
+    public double clawPoz = close;
+    public double wristPlace = 0.1;
+    public double maxVel = 25;
 
     public class ArmThread extends Thread{
         ArmControler brat;
         public ArmThread(ArmControler brat){
             this.brat = brat;
 
-            brat.setPower(0.5);
+            brat.setWrist(0);
+            brat.setPower(1);
             brat.setAxPoz(targetAx);
             brat.setSliderPoz(targetSlider);
             brat.setClaw(close);
@@ -43,7 +46,7 @@ public class RegioRight extends LinearOpMode {
                 brat.setAxPoz(targetAx);
                 brat.setSliderPoz(targetSlider);
                 brat.setClaw(clawPoz);
-                brat.setWristParalel(wristPlace,1);
+                brat.setWrist(wristPlace);
 
                 brat.callTelemetry();
                 telemetry.update();
@@ -58,39 +61,46 @@ public class RegioRight extends LinearOpMode {
 
         TrajectorySequence regioRight = drive.trajectorySequenceBuilder(startingPoseRegioRight)
                 .UNSTABLE_addTemporalMarkerOffset(0, ()->{
-                    targetAx = 400;
-                    targetSlider = 900;
-                    wristPlace = 0.1;
+                    targetAx = 450;
+                    targetSlider = 1400;
+                    wristPlace = 0.6;
                 })
-                .splineToSplineHeading(new Pose2d(10, -28, Math.toRadians(90)), Math.toRadians(90))
-                .UNSTABLE_addTemporalMarkerOffset(0.2, ()->{
-                    clawPoz = open;
+                .waitSeconds(0.8)
+                .splineToSplineHeading(new Pose2d(10, -26, Math.toRadians(90)), Math.toRadians(90))
+                .waitSeconds(2)
+                .UNSTABLE_addTemporalMarkerOffset(-1, ()->{
+                    clawPoz = 0.4;
                 })
-                .UNSTABLE_addTemporalMarkerOffset(1, ()->{
+                .UNSTABLE_addTemporalMarkerOffset(-0.5, ()->{
+                    targetAx = 390;
                     targetSlider = 0;
                 })
-                .UNSTABLE_addTemporalMarkerOffset(1.8, ()->{
-                    targetAx = 20;
-                    wristPlace = 0.4;
-                })
-                .waitSeconds(3)
+                .waitSeconds(0.5)
                 .setTangent(Math.toRadians(-10))
                 .splineToSplineHeading(new Pose2d(34, -38, Math.toRadians(270)), Math.toRadians(30))
-                .setVelConstraint(new TranslationalVelocityConstraint(maxVel))
+                .setVelConstraint(new TranslationalVelocityConstraint(maxVel - 5))
                 .splineToConstantHeading(new Vector2d(38, -15), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(48, -15), Math.toRadians(270))
                 .resetVelConstraint()
                 .splineToConstantHeading(new Vector2d(48, -45), Math.toRadians(270))
-                .setVelConstraint(new TranslationalVelocityConstraint(maxVel))
+                .setVelConstraint(new TranslationalVelocityConstraint(maxVel - 5))
                 .splineToConstantHeading(new Vector2d(48, -15), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(59, -15), Math.toRadians(270))
                 .resetVelConstraint()
                 .splineToConstantHeading(new Vector2d(59, -45), Math.toRadians(270))
-                .setVelConstraint(new TranslationalVelocityConstraint(maxVel))
+                .setVelConstraint(new TranslationalVelocityConstraint(maxVel - 5))
                 .splineToConstantHeading(new Vector2d(59, -15), Math.toRadians(90))
                 .splineToConstantHeading(new Vector2d(66, -15), Math.toRadians(270))
                 .resetVelConstraint()
                 .splineToConstantHeading(new Vector2d(66, -45), Math.toRadians(270))
+                .UNSTABLE_addTemporalMarkerOffset(-1, ()->{
+                    targetAx = 150;
+                    targetSlider = 200;
+                })
+                .waitSeconds(0.5)
+                .UNSTABLE_addTemporalMarkerOffset(-0.1, ()->{
+                    clawPoz = 0.5;
+                })
                 .setTangent(Math.toRadians(180))
                 .splineToConstantHeading(new Vector2d(48, -47), Math.toRadians(180))
                 .setTangent(Math.toRadians(160))
@@ -109,8 +119,5 @@ public class RegioRight extends LinearOpMode {
 
         sleep(500);
         drive.followTrajectorySequence(regioRight);
-        while(!isStopRequested()){
-
-        }
     }
 }
