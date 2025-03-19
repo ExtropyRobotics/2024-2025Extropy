74d08be08d6f5d10693d179b8042e3763afe5bbd
Dettar Daniel <ocrisan23@gmail.com>
2025-03-19 22:18:23 +0200

    initial pedro + some tests

---
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -0,0 +1,52 @@
+package pedroPathing.constants;
+
+import com.pedropathing.localization.Localizers;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.util.CustomFilteredPIDFCoefficients;
+import com.pedropathing.util.CustomPIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class FConstants {
+    static {
+        FollowerConstants.localizers = Localizers.THREE_WHEEL;
+
+        FollowerConstants.leftFrontMotorName = "leftFront";
+        FollowerConstants.leftRearMotorName = "leftBack";
+        FollowerConstants.rightFrontMotorName = "rightFront";
+        FollowerConstants.rightRearMotorName = "rightBack";
+
+        FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
+        FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
+        FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
+
+        FollowerConstants.mass = 4.7;
+
+        FollowerConstants.xMovement = 56.708716923784706;
+        FollowerConstants.yMovement = 43.2867979536244;
+
+        FollowerConstants.forwardZeroPowerAcceleration = -61.75;
+        FollowerConstants.lateralZeroPowerAcceleration = -72.76885760695573;
+
+        FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.2,0,0.03,0);
+        FollowerConstants.useSecondaryTranslationalPID = false;
+        FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
+
+        FollowerConstants.headingPIDFCoefficients.setCoefficients(3,0,0.2,0);
+        FollowerConstants.useSecondaryHeadingPID = false;
+        FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
+
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.01,0,0.001,0.6,0);
+        FollowerConstants.useSecondaryDrivePID = false;
+        FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
+
+        FollowerConstants.zeroPowerAccelerationMultiplier = 1;
+        FollowerConstants.centripetalScaling = 0.0005;
+
+        FollowerConstants.pathEndTimeoutConstraint = 500;
+        FollowerConstants.pathEndTValueConstraint = 0.995;
+        FollowerConstants.pathEndVelocityConstraint = 0.1;
+        FollowerConstants.pathEndTranslationalConstraint = 0.1;
+        FollowerConstants.pathEndHeadingConstraint = 0.007;
+    }
+}
---
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -0,0 +1,21 @@
+package pedroPathing.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+
+public class LConstants {
+    static {
+        ThreeWheelConstants.forwardTicksToInches = 0.0019715;
+        ThreeWheelConstants.strafeTicksToInches = 0.002;
+        ThreeWheelConstants.turnTicksToInches = 0.001932;
+        ThreeWheelConstants.leftY = 10.7/2.54;
+        ThreeWheelConstants.rightY = -10.7/2.54;
+        ThreeWheelConstants.strafeX = 8.5/2.54;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightFront";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightBack";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
+    }
+}
