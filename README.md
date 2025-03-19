7cd713949b67d5cc049e9599988dacf2dfb37f13
Dettar Daniel <ocrisan23@gmail.com>
2025-03-19 23:14:15 +0200

    PowerPlay codes

--- README.md
+++
@@ -1,3 +0,0 @@
-# Done using Road Runner Quickstart
-
-- Extropy codes for 2023-2024 season.
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/State/ActionState.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.modules.State;
-
-public enum ActionState {
-    ONGOING,
-    FINISHED
-}
